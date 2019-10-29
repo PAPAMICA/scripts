386510259ae6a758bb3ff1e09287104c7c6b49c7
PAPAMICA <asselinemickael@gmail.com>
2019-10-29 14:21:36 +0100

    Update debian_zabbix_agent.sh

--- debian_zabbix_agent.sh
+++ debian_zabbix_agent.sh
@@ -4,13 +4,13 @@ read -p "Entrez le nom du serveur : " server_name
 dpkg -i zabbix-release_4.0-3+stretch_all.deb
 
 apt-get update
-apt-get install zabbix-agent
+apt-get install zabbix-agent -y
 
 for file in /etc/zabbix/zabbix_agentd.conf
 do
   echo "Traitement de $file ..."
   sed -i -e "s/Server=127.0.0.1/Server=188.213.26.14/g" "$file"
-  sed -i -e "s/Hostname=/Hostname=$server_name/g" "$file"
+  sed -i -e "s/Hostname=Zabbix server/Hostname=$server_name/g" "$file"
 done
 
 service zabbix-agent start
