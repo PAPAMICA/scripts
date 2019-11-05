b514c7a23bd9249c6c26aaaf6ded04f5f06fa112
PAPAMICA <asselinemickael@gmail.com>
2019-11-05 12:33:59 +0100

    Update debian_install_zabbix_server.sh

--- debian_install_zabbix_server.sh
+++ debian_install_zabbix_server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 clear
 # Principaux paramètres
-read -p "Entrez le mot de passe pour la base de données Zabbix : " ZABBIX_DB_USER_PASSWORD
+tput setaf 2; read -p "Entrez le mot de passe pour la base de données Zabbix : " ZABBIX_DB_USER_PASSWORD
 #read -p "Entrez l'adresse ip du serveur : " SERVER_IP
 SERVER_IP=$(hostname -i)
+echo ""
 
 # Installation des dépendances et de docker
 apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
@@ -14,29 +15,30 @@ apt-get -y install docker-ce docker-compose
 systemctl enable docker
 systemctl start docker
 
-# Récupération des images de Zabbix et de Grafana
-docker pull zabbix/zabbix-server-pgsql:ubuntu-latest
-docker pull grafana/grafana
-docker pull postgres:latest
-
-# Lancement du docker-compose.yml
-cd zabbix
-for file in zabbix/docker-compose.yml
+# Modification et lancement du docker-compose.yml
+for file in ~/scripts/zabbix/docker-compose.yml
 do
   echo "Traitement de $file ..."
   sed -i -e "s/zabbix-bdd-password/$ZABBIX_DB_USER_PASSWORD/g" "$file"
   sed -i -e "s/ip-adress-zabbix/$SERVER_IP/g" "$file"
 done
+cd ~/scripts/zabbix/
 docker-compose up -d
 
 clear
-
 tput bold; tput setaf 7; echo "LISTES DES CONTAINERS EN COURS : "
 docker container ls
 tput setaf 7; echo "-------------------------------------------------"
-tput bold; tput setaf 7; echo "      => INSTALLATION TERMINEE <="
+tput bold; tput setaf 7; echo "        => INSTALLATION TERMINEE <="
+tput setaf 7; echo ""
+tput setaf 7; echo "     IP du serveur Zabbix : $SERVER_IP:8090      "
+tput setaf 7; echo "           ID : Admin / MDP : zabbix             "
+tput setaf 7; echo ""
+tput setaf 7; echo "     IP du serveur Grafana : $SERVER_IP:3000     "
+tput setaf 7; echo "           ID : admin / MDP : admin              "
 tput setaf 7; echo ""
 tput bold; tput setaf 6; echo "                By PAPAMICA"
+tput bold; tput setaf 6; echo "                Labo-Tech.fr"
 tput setaf 7; echo "-------------------------------------------------"
-echo ""
+tput setaf 2; echo ""
 
